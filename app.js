0b016c7f8995128e88b375ee981b8bf604661845
stormymayday <stormymayday7@gmail.com>
2023-04-03 19:35:57 -0600

    Selecting the '.main-img' inside the Modal

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ function Gallery(element) {
     // 1. Getting the NodeList of all '.img' for that element
     // 2. Using Spread Operator to convert NodeList into an Array
     this.list = [...element.querySelectorAll('.img')];
-    console.log(this.list);
 
+    // Targetting the Modal
+    this.modal = getElement('.modal');
+
+    // Targetting the '.main-img' inside the Modal
+    this.mainImg = getElement('.main-img');
+    console.log(this.mainImg);
 
 }
 // Constructor function - end
