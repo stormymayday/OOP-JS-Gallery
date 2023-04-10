a1fd8aa4c034040bd0e09ed40320ececb114027d
stormymayday <stormymayday7@gmail.com>
2023-04-09 20:14:43 -0600

    Added the getElement function

--- images/app-class.js
+++ images/app-class.js
@@ -12,3 +12,16 @@ class Gallery {
 
 }
 // Gallery class - end
+
+// getElement - start
+function getElement(selection) {
+
+    const element = document.querySelector(selection);
+
+    if (element) {
+        return element;
+    }
+
+    throw new Error(`Please check "${selection}" selector, no such element exists`);
+}
+// getElement - end
