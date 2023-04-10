188f4acd2fcfdb5019660c23920973450d298751
stormymayday <stormymayday7@gmail.com>
2023-04-09 20:38:29 -0600

    Instantiating the City Gallery

--- images/app-class.js
+++ images/app-class.js
@@ -223,4 +223,6 @@ function getElement(selection) {
 // Nature Gallery:
 const natureGallery = new Gallery(getElement('.nature'));
 
+// City Gallery:
+const cityGallery = new Gallery(getElement('.city'));
 // Gallery Instances - end
