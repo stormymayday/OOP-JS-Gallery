f014bdbfb28382059c4b29a5411f3e184eaf117d
stormymayday <stormymayday7@gmail.com>
2023-04-09 20:13:04 -0600

    Defined the Gallery class

---
+++ images/app-class.js
@@ -0,0 +1,14 @@
+// Gallery class - start
+class Gallery {
+
+    // constructor - start
+    constructor() {
+
+    }
+    // constructor - end
+
+    // Methods - start
+    // Methods - end
+
+}
+// Gallery class - end
